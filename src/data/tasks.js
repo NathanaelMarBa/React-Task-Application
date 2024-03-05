66fed3b4c61afee5a41f53abaec5f14a9c388fa9
Nathanael <kevin_1321124060@uptecamac.edu.mx>
2024-03-05 13:05:37 -0600

    Implementacion de mejoras

--- src/data/tasks.js
+++ src/data/tasks.js
@@ -1,17 +1,3 @@
 export const tasks = [
-  {
-    id: 0,
-    title: "Mi Primer tarea",
-    description: "Esto es una tarea",
-  },
-  {
-    id: 1,
-    title: "Mi Segunda tarea",
-    description: "Esto es una tarea",
-  },
-  {
-    id: 2,
-    title: "Mi Tercer tarea",
-    description: "Esto es una tarea",
-  },
+  
 ];
